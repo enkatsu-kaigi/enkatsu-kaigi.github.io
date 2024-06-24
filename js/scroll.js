59f7feaced2af5392d08938d3afcbf826081cfb5
enkatsu-kaigi <127960321+enkatsu-kaigi@users.noreply.github.com>
2024-06-25 00:25:23 +0900

    Update

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,97 @@
 //スクロールのためのスクリプト　https://mitsutano-oshiro.com/archives/1842　//
+//actor-a//
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".actor-a").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 500) {
+                $(this).addClass("fadeIn-side");
+            }
+        });
+    });
+});
+//actor-b//
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".actor-b").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 500) {
+                $(this).addClass("fadeIn-side");
+            }
+        });
+    });
+});
+//actor-c//
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".actor-c").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 500) {
+                $(this).addClass("fadeIn-side");
+            }
+        });
+    });
+});
+//actor-d//
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".actor-d").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 500) {
+                $(this).addClass("fadeIn-side");
+            }
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 //actor1//
 $(function () {
     // ウィンドウをスクロールしたら…
@@ -13,7 +106,7 @@ $(function () {
                 const bPosition = $(this).offset().top;
                 // スクロールした量が要素の高さを上回ったら
                 // その数値にウィンドウの高さを引き、最後に200pxを足す
-            if (wScroll > bPosition - wHeight + 200) {
+            if (wScroll > bPosition - wHeight + 300) {
                 $(this).addClass("fadeIn");
             }
         });
