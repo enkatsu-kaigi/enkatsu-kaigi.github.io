f1b0dfa19fd6b7876d39a4b5d0838eda4dec5032
enkatsu-kaigi <127960321+enkatsu-kaigi@users.noreply.github.com>
2024-06-26 19:59:37 +0900

    Update

--- js/scroll.js
+++ js/scroll.js
@@ -196,3 +196,24 @@ $(function () {
         });
     });
 });
+
+//actor6//
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".actor6").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 200) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    });
+});
