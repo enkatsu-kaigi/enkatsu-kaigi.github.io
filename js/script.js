9886164d8346acd02d3b6eb295a115c433230f4b
enkatsu-kaigi <127960321+enkatsu-kaigi@users.noreply.github.com>
2024-06-26 23:43:08 +0900

    Update

--- js/script.js
+++ js/script.js
@@ -10,4 +10,24 @@ $(function() {
   });
 });
 
-
+//スクロールのためのスクリプト　https://mitsutano-oshiro.com/archives/1842　//
+//キャッチコピー//
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".catch-copy").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    });
+});
